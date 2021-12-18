e9253c9c06204e90392a67daf4d9b14eccc863b3
gfnvvlntn <gfnvvlntn@gmail.com>
2021-12-18 22:14:54 +0300

    Добавил сохранение в localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
 import todoReducer from "./slice";
+
+const localStorageMiddleware: Middleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem("todoList", JSON.stringify(getState()));
+    return result;
+  };
+};
+
+const reHydrateStore = () => {
+  if (localStorage.getItem("todoList") !== null) {
+    return JSON.parse(localStorage.getItem("todoList") || "");
+  }
+};
 
 const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+  devTools: true,
 });
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
